7fc82549f470294f73f6e73952c0cb1ff84e03e0
Giovanni Donati <grdonati99@gmail.com>
2024-09-26 06:13:20 -0300

    Pesquisas em Set

---
+++ src/main/java/collections/set/pesquisa/AgendaContatos.java
@@ -0,0 +1,56 @@
+package main.java.collections.set.pesquisa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class AgendaContatos {
+    private Set<Contato> contatoSet;
+
+    public AgendaContatos() {
+        this.contatoSet = new HashSet<>();
+    }
+
+    public void adicionarContato(String nome, int numero){
+        contatoSet.add(new Contato(nome, numero));
+    }
+
+    public void exibirContatos(){
+        System.out.println(contatoSet);
+    }
+
+    public Set<Contato>pesquisarPorNome(String nome){
+        Set<Contato> contatosPorNome = new HashSet<>();
+        for(Contato c : contatoSet){
+            if(c.getNome().startsWith(nome)){
+                contatosPorNome.add(c);
+            }
+        }
+        return contatosPorNome;
+    }
+
+    public Contato atualizarNumeroContato(String nome, int novoNumero){
+        Contato contatoAtualizado = null;
+        for(Contato c : contatoSet){
+            if(c.getNome().equalsIgnoreCase(nome)){
+                c.setNumero(novoNumero);
+                contatoAtualizado = c;
+                break;
+            }
+        }
+        return contatoAtualizado;
+    }
+
+    public static void main(String[] args) {
+        AgendaContatos agendaContatos = new AgendaContatos();
+        agendaContatos.adicionarContato("Kemili", 123456789);
+        agendaContatos.adicionarContato("Malia", 987654321);
+        agendaContatos.adicionarContato("Giovanni", 456789123);
+
+        agendaContatos.exibirContatos();
+        System.out.println(agendaContatos.pesquisarPorNome("Giovanni"));
+
+        agendaContatos.adicionarContato("Giovanni Donati",456789123);
+        agendaContatos.atualizarNumeroContato("Giovanni", 123456789);
+        agendaContatos.exibirContatos();
+    }
+}
---
+++ src/main/java/collections/set/pesquisa/ListaTarefas.java
@@ -0,0 +1,4 @@
+package main.java.collections.set.pesquisa;
+
+public class ListaTarefas {
+}

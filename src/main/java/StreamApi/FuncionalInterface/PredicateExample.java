a66f7cd3b804daeb04bbbf9823e1418f97526f5d
Giovanni Donati <grdonati99@gmail.com>
2025-02-03 21:43:37 -0300

    Other examples FunctionalInterface

---
+++ src/main/java/StreamApi/FuncionalInterface/PredicateExample.java
@@ -0,0 +1,17 @@
+package main.java.StreamApi.FuncionalInterface;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class PredicateExample {
+    public static void main(String[] args) {
+        List<String> palavras = Arrays.asList("Java", "Python", "Kotlin", "Js");
+
+        Predicate<String> maisDeCincoCaracteres = palavra -> palavra.length() > 5;
+
+        palavras.stream()
+                .filter(maisDeCincoCaracteres)
+                .forEach(System.out::println);
+    }
+}
